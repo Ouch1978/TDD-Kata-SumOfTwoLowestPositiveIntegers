2b766a011c20024675e35623f42dd1120660b2be
Ouch.Liu <ouch.liu@xuenn.com>
2018-06-05 11:02:43 +0800

    Initial commit

---
+++ SumOfTwoLowestPositiveIntegers/SumOfTwoLowestPositiveIntegersTests/KataTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumOfTwoLowestPositiveIntegers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoLowestPositiveIntegers.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void SumTwoSmallestNumbersTest()
+        {
+            int[] numbers = { 5 , 8 , 12 , 19 , 22 };
+            Assert.AreEqual( 13 , Kata.SumTwoSmallestNumbers( numbers ) );
+        }
+    }
+}
